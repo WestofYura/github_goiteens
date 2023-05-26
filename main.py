dbf64e48c31bc565d162c7da442a6f27eb76546d
Yurii Sorokvashyn <westofyura228@gmail.com>
2023-05-26 17:07:39 +0300

    new file + modified main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from time import sleep
 
 
 height = input("Якого ти зросту?")
+age = input("скільки тобі років??")
 sleep(2)
 print("loading")
 sleep(2)
-print(f"Ти {height}см зросту")
+print(f"Ти {height}см зросту та тобі {age} років")
